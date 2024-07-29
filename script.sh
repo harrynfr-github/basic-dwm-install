3af2404229892aa78ac7c650f5a1a7a52d907220
HarryNFR <harrynfr@mailfence.com>
2024-07-29 20:00:08 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,27 +7,38 @@ if [ -z "$USERNAME" ]; then
   USERNAME="root"
 fi
 
-pacman -S base-devel git xorg-server xorg-xinit xorg-xrandr libx11 libxinerama libxft webkit2gtk
+pacman -S base-devel git xorg-server xorg-xinit xorg-xrandr libx11 libxinerama libxft webkit2gtk --noconfirm
 
-mkdir -p /home/$USERNAME/suckless
-cd /home/$USERNAME/suckless
+if [ "$USERNAME" == "root" ]; then
+  HOME_DIR="/root"
+else
+  HOME_DIR="/home/$USERNAME"
+fi
+
+mkdir -p $HOME_DIR/suckless
+cd $HOME_DIR/suckless
 git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/st
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/slstatus
 
-cd /home/$USERNAME/suckless/st
+cd $HOME_DIR/suckless/st
 make clean install
-cd /home/$USERNAME/suckless/dwm
+cd $HOME_DIR/suckless/dwm
 make clean install
-cd /home/$USERNAME/suckless/dmenu
+cd $HOME_DIR/suckless/dmenu
 make clean install
-cd /home/$USERNAME/suckless/slstatus
+cd $HOME_DIR/suckless/slstatus
 make clean install
 
-echo "slstatus &" >> /home/$USERNAME/.xinitrc
-echo "exec dwm" >> /home/$USERNAME/.xinitrc
+echo "slstatus &" >> $HOME_DIR/.xinitrc
+echo "exec dwm" >> $HOME_DIR/.xinitrc
+echo "startx" >> $HOME_DIR/.bash_profile
 
-echo "startx" >> /home/$USERNAME/.bash_profile
+if [ "$USERNAME" != "root" ]; then
+  chown -R $USERNAME:$USERNAME $HOME_DIR/suckless
+  chown $USERNAME:$USERNAME $HOME_DIR/.xinitrc
+  chown $USERNAME:$USERNAME $HOME_DIR/.bash_profile
+fi
 
 reboot now
